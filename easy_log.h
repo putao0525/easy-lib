e25aff4a2f18cc3319bb9f07e0fb14ac7c8d2318
putao0525 <811458094@qq.com>
2024-09-02 15:30:31 +0800

    upate

--- easy_log.h
+++ easy_log.h
@@ -5,6 +5,19 @@
 
 #ifndef EASY_LIB_EASY_LOG_H
 #define EASY_LIB_EASY_LOG_H
+#include <time.h>
+
+// 获取当前时间并格式化
+#define CURRENT_TIME() ({ \
+    time_t rawtime; \
+    struct tm * timeinfo; \
+    static char buffer[20]; \
+    time(&rawtime); \
+    timeinfo = localtime(&rawtime); \
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo); \
+    buffer; \
+})
+
 // 定义颜色代码
 #define COLOR_RED     "\033[31m"
 #define COLOR_GREEN   "\033[32m"
@@ -27,15 +40,16 @@
 
 // 内部使用的日志宏
 #define PRIVATE_EASY_LOG(level, fmt, ...) do { \
+    const char* level_str; \
     const char* color; \
     switch (level) { \
-        case EASY_LOG_LEVEL_DEBUG: color = COLOR_CYAN; break; \
-        case EASY_LOG_LEVEL_INFO:  color = COLOR_GREEN; break; \
-        case EASY_LOG_LEVEL_WARN:  color = COLOR_YELLOW; break; \
-        case EASY_LOG_LEVEL_ERROR: color = COLOR_RED; break; \
-        default: color = COLOR_RESET; break; \
+        case EASY_LOG_LEVEL_DEBUG: level_str = "DEBUG"; color = COLOR_CYAN; break; \
+        case EASY_LOG_LEVEL_INFO:  level_str = "INFO"; color = COLOR_GREEN; break; \
+        case EASY_LOG_LEVEL_WARN:  level_str = "WARN"; color = COLOR_YELLOW; break; \
+        case EASY_LOG_LEVEL_ERROR: level_str = "ERROR"; color = COLOR_RED; break; \
+        default: level_str = "UNKNOWN"; color = COLOR_RESET; break; \
     } \
-    printf("%s[%s:%d] " fmt "%s\n", color, __FILE__, __LINE__, ##__VA_ARGS__, COLOR_RESET); \
+    printf("%s [%s:%d] %s[%s]%s " fmt "\n", CURRENT_TIME(), __FILE__, __LINE__, color, level_str, COLOR_RESET, ##__VA_ARGS__); \
 } while (0)
 
 #endif //EASY_LIB_EASY_LOG_H
--- main.c
+++ main.c
@@ -6,7 +6,10 @@ void testNumber() {
     int a = 5;
     int b = 3;
     int c = NUM_MIN(a, b);
-//    EASY_LOG("最小值：%d", c);
+    EASY_LOG_INFO("最小值：%d", c);
+    EASY_LOG_ERROR("最小值不符合预期：%d", c);
+    EASY_LOG_WARN("最小值不符合预期：%d", c);
+    EASY_LOG_DEBUG("最小值不符合预期：%d", c);
 }
 
 void testLog() {
